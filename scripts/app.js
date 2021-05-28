90c88a612b2d84cbbdb184ae7e9b8edd8a888b6d
simplythebex <becca.jones@live.co.uk>
2021-05-28 12:35:46 +0100

    Added first ghost, added borders, added score and lives

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,36 @@
 function init() {
   // variables
   const grid = document.querySelector('.grid')
-  const width = 21
+  const width = 11
   const cellCount = width * width
   const cells = []
 
+  // player info
   const playerClass = 'player'
-  const borderClass = 'border'
-  const playerStartPosition = 22
-  let playerCurrentPosition = 22
+  const playerStartPosition = 12
+  let playerCurrentPosition = 12
 
+  // first ghost info
+  const ghostClass = 'ghost'
+  const ghostStartPosition = 65
+  let ghostCurrentPosition = 65
+
+  // level
   const level = 'beginner'
 
-  const borderArray = []
+  // border info
+  const borderClass = 'border'
+  const borderArray = [24, 25, 26, 28, 29, 30, 45, 46, 48, 50, 52, 53, 59, 61, 67, 68, 70, 71, 72, 74, 75, 90, 91, 92, 94, 95, 96]
+
+  // food info
+  const foodClass = 'food'
+  let score = 0
+  const grabScore = document.querySelector('.score span')
+
+  // fossil info
+  const fossilClass = 'fossil'
+  let lives = 2
+  const grabLives = document.querySelector('.lives span')
 
   // functions
 
@@ -20,31 +38,44 @@ function init() {
   function createGrid(playerStartPosition) {
     for (let i = 0; i < cellCount; i++) {
       const cell = document.createElement('div')
-      cell.innerText = i
+      // cell.innerText = i
       grid.appendChild(cell)
       cells.push(cell)
       // generate borders and map
       if (level === 'beginner') {
-        if (i < width || (i % width === 0 && i !== 210) || (i % width === width - 1 && i !== 230) || i + width > width * width - 1 || borderArray.includes(i)) {
-          cell.classList.add('border')
+        if (i < width || (i % width === 0 && i !== 55) || (i % width === width - 1 && i !== 65) || i + width > width * width - 1 || borderArray.includes(i)) {
+          cell.classList.add(borderClass)
+        } else if (i === 82) {
+          cell.classList.add(fossilClass)
+        } else if (!cell.classList.contains(borderClass) && !cell.classList.contains(fossilClass)) {
+          console.log(cell)
+          cell.classList.add(foodClass)
         }
       }
     }
     addPlayer(playerStartPosition)
+    // addFirstGhost(ghostStartPosition)
+    // moveGhost()
   }
 
   // add the player 
   function addPlayer(position) {
-    console.log('position passed in ->', position)
-    console.log('cell being used', cells[position])
     cells[position].classList.add(playerClass)
   }
 
   // remove the player
   function removePlayer(position) {
-    console.log('position passed in ->', position)
-    console.log('position being used ->', cells[position])
     cells[position].classList.remove(playerClass)
+  }
+
+  // add first ghost
+  function addFirstGhost(position) {
+    cells[position].classList.add(ghostClass)
+  }
+
+  // remove first ghost
+  function removeFirstGhost(position) {
+    cells[position].classList.remove(ghostClass)
   }
 
   // move the player
@@ -56,24 +87,95 @@ function init() {
 
     if (key === 38 && !cells[playerCurrentPosition - width].classList.contains(borderClass)) { // up
       playerCurrentPosition -= width
-    } else if (key === 39 && (!cells[playerCurrentPosition + 1].classList.contains(borderClass) || playerCurrentPosition === 230)) { // right
-      if (playerCurrentPosition === 230) {
-        playerCurrentPosition = 210
+      eatFruit(playerCurrentPosition)
+      getFossil(playerCurrentPosition)
+    } else if (key === 39 && (!cells[playerCurrentPosition + 1].classList.contains(borderClass) || playerCurrentPosition === 65)) { // right
+      if (playerCurrentPosition === 65) {
+        playerCurrentPosition = 55
+        eatFruit(playerCurrentPosition)
+        getFossil(playerCurrentPosition)
       } else {
         playerCurrentPosition ++
+        eatFruit(playerCurrentPosition)
+        getFossil(playerCurrentPosition)
       }
     } else if (key === 40 && !cells[playerCurrentPosition + width].classList.contains(borderClass)) { // down
       playerCurrentPosition += width
-    } else if (key === 37 && (!cells[playerCurrentPosition - 1].classList.contains(borderClass) || playerCurrentPosition === 210)) {// left
-      if (playerCurrentPosition === 210) {
-        playerCurrentPosition = 230
+      eatFruit(playerCurrentPosition)
+      getFossil(playerCurrentPosition)
+    } else if (key === 37 && (!cells[playerCurrentPosition - 1].classList.contains(borderClass) || playerCurrentPosition === 55)) {// left
+      if (playerCurrentPosition === 55) {
+        playerCurrentPosition = 65
+        eatFruit(playerCurrentPosition)
+        getFossil(playerCurrentPosition)
       } else {
         playerCurrentPosition--
+        eatFruit(playerCurrentPosition)
+        getFossil(playerCurrentPosition)
       }
     }
     console.log('position after moving ->', playerCurrentPosition)
     addPlayer(playerCurrentPosition)
   }
+
+  // move the ghost
+  function moveGhost() {
+    const randomIndex = Math.floor(Math.random() * 4)
+    removeFirstGhost(ghostCurrentPosition)
+    console.log(ghostCurrentPosition)
+    if (randomIndex === 0 && !cells[ghostCurrentPosition - width].classList.contains(borderClass)) {
+      ghostCurrentPosition -= width
+    } else if (randomIndex === 1 && (!cells[ghostCurrentPosition + 1].classList.contains(borderClass) || ghostCurrentPosition === 65)) { // right
+      if (ghostCurrentPosition === 65) {
+        ghostCurrentPosition = 55
+      } else {
+        ghostCurrentPosition ++
+      }
+    } else if (randomIndex === 2 && !cells[ghostCurrentPosition + width].classList.contains(borderClass)) { // down
+      ghostCurrentPosition += width
+    } else if (randomIndex === 3 && (!cells[ghostCurrentPosition - 1].classList.contains(borderClass) || ghostCurrentPosition === 55)) {// left
+      if (ghostCurrentPosition === 55) {
+        ghostCurrentPosition = 65
+      } else {
+        ghostCurrentPosition--
+      }
+    } 
+    // addFirstGhost(ghostCurrentPosition)
+  }
+
+  // call move ghost
+  // const move = setInterval(() => {
+  //   moveGhost()
+  // }, 300)
+
+  // eat fruit
+  function eatFruit(playerCurrentPosition) {
+    if (cells[playerCurrentPosition].classList.contains(foodClass)) {
+      cells[playerCurrentPosition].classList.remove(foodClass)
+      score++
+      updateScore(score)
+    }
+  }
+
+  // update score
+  function updateScore(score) {
+    grabScore.innerText = score
+  }
+
+  // get fossil
+  function getFossil(playerCurrentPosition) {
+    if (cells[playerCurrentPosition].classList.contains(fossilClass)) {
+      cells[playerCurrentPosition].classList.remove(fossilClass)
+      lives++
+      updateLives(lives)
+    }
+  }
+
+  // update lives
+  function updateLives(lives) {
+    grabLives.innerText = lives
+  }
+
 
   createGrid(playerStartPosition)
 
